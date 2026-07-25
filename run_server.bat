8ca4dad42cc6e5a0a7a279da3ca99ec87914de7e
Mustafa ayverdi <locko6634@gmail.com>
2026-07-26 01:12:34 +0300

    0dan başlanıcak.

--- run_server.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title Python HTTP Server Baslatici
-cd /d "%~dp0"
-
-echo Bagimsiz Python sunucusu tetikleniyor...
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0sunucu_baslat.ps1"
-pause
