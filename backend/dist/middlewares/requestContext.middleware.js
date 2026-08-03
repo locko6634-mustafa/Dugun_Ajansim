d83888e849a3143c44157e93630447b9068f744f
Mustafa ayverdi <locko6634@gmail.com>
2026-08-03 09:36:54 +0300

    chore: remove tracked reports, attachments and dist files from git index

--- backend/dist/middlewares/requestContext.middleware.js
+++
@@ -1,8 +0,0 @@
-import { randomUUID } from 'node:crypto';
-export const attachRequestContext = (req, res, next) => {
-    const incomingId = req.get('X-Correlation-ID');
-    req.correlationId =
-        incomingId && /^[A-Za-z0-9._-]{8,128}$/.test(incomingId) ? incomingId : randomUUID();
-    res.set('X-Correlation-ID', req.correlationId);
-    next();
-};
